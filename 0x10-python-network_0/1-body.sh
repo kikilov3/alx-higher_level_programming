6b326a4398b0107bb9870735765e53cefb7e56c5
kikilov3 <111880553+kikilov3@users.noreply.github.com>
2023-04-13 00:32:25 +0100

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-response=$(curl -sw '%{http_code}' "$1")
-status="${response:${#response}-3}"
-if [ "$status" -eq 200 ]; then
-  body=$(curl -s "$1")
-  echo "$body"
-fi
+# script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -Ls "$1"
