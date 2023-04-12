01ee3e3cf80b8a5ebc49122d4810c35a96bcb2c8
kikilov3 <111880553+kikilov3@users.noreply.github.com>
2023-04-13 00:34:00 +0100

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -Ls "$1"
+# Get the response body for a given URL for 200 status code responses.
+curl -sL "$1"
